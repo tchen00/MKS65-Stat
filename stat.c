92862a8fd0528e2c046735e26e675c48b881abd1
Tammy Chen <tchen00@stuy.edu>
2019-11-13 12:52:17 -0500

    add fxn

--- stat.c
+++ stat.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <time.h>
+
+char * readable(long s){
+  int i = 0;
+  char * form[] = {"B", "KB", "MB", "GB"};
+  while (s >= 1024){
+    s /= 1024;
+    i++;
+  }
+  return form[i];
+}
+
+void permission(mode_t mode, char * buffer){
+	char perm[10] ="rwxrwxrwx";
+	for (int i = 0; i < 9; i++){
+		if(!(mode & (1 << (8-i)))){
+			perm[i] = '-';
+		}
+	}
+	strcat(buffer, perm);
+}
 
 int main(){
   struct stat file;
